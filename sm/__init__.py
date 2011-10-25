f6bfe66a6ba37bfffc38454687ca13a93367e85a
Christopher Grebs <cg@webshox.org>
2011-10-25 07:05:58 +0200

    new relic

--- sm/__init__.py
+++ sm/__init__.py
@@ -1,4 +1,4 @@
 import os
 
 import newrelic.agent
-newrelic.agent.initialize(os.path.join(os.path.abspath(os.path.dirname(__name__), 'newrelic.ini')))
+newrelic.agent.initialize(os.path.join(os.path.abspath(os.path.dirname(__name__)), 'newrelic.ini'))
